0dd68b44e617ed80be5c304b74ceb1747b4683bd
Martín Coll <mgonzalezcoll@fi.uba.ar>
2020-11-01 17:02:40 -0300

    mapLineToRow

--- src/main/scala/fiuba/fp/Run.scala
+++ src/main/scala/fiuba/fp/Run.scala
@@ -1,10 +1,29 @@
 package fiuba.fp
 
-import cats.effect._
+import cats.effect.{Blocker, ExitCode, IO, IOApp}
+import fs2.{Stream, io, text}
+import java.nio.file.Paths
+import java.time.LocalDateTime
 
-object Run extends App {
+import fiuba.fp.models.DataSetRow
 
-    val program = IO { println(s"Hello!") }
-    
-    program.unsafeRunSync() 
+object Run extends IOApp {
+
+    val converter: Stream[IO, Unit] = Stream.resource(Blocker[IO]).flatMap { blocker =>
+        io.file.readAll[IO](Paths.get("train.csv"), blocker, 4096)
+          .through(text.utf8Decode)
+          .through(text.lines)
+          .drop(1) // remove header
+          .map(mapLineToRow)
+          .map(_.toString)
+          .intersperse("\n")
+          .through(text.utf8Encode)
+          .through(io.file.writeAll(Paths.get("celsius.txt"), blocker))
+    }
+
+    def run(args: List[String]): IO[ExitCode] =
+        converter.compile.drain.map(_ => ExitCode.Success)
+
+    def mapLineToRow(line: String): Option[DataSetRow] = Some(DataSetRow(
+        1, LocalDateTime.MIN, None, None, None, 0, 0, 0, "", None, None, None, "", 0, 0, 0))
 }
