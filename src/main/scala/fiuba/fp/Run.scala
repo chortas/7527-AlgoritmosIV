724a79e1e0f508912cf417cd463b6e639b810691
Cecilia Hortas <chortas@fi.uba.ar>
2020-11-01 17:18:39 -0300

    parse Id

--- src/main/scala/fiuba/fp/Run.scala
+++ src/main/scala/fiuba/fp/Run.scala
@@ -9,30 +9,54 @@ import fiuba.fp.models.DataSetRow
 
 object Run extends IOApp {
 
-    val converter: Stream[IO, Unit] = Stream.resource(Blocker[IO]).flatMap { blocker =>
-        io.file.readAll[IO](Paths.get("train.csv"), blocker, 4096)
-          .through(text.utf8Decode)
-          .through(text.lines)
-          .drop(1) // remove header
-          .dropLastIf(_.isEmpty)
-          .map(mapLineToRow)
-          .map(_.toString)
-          .intersperse("\n")
-          .through(text.utf8Encode)
-          .through(io.file.writeAll(Paths.get("celsius.txt"), blocker))
+  val converter: Stream[IO, Unit] = Stream.resource(Blocker[IO]).flatMap {
+    blocker =>
+      io.file
+        .readAll[IO](Paths.get("train.csv"), blocker, 4096)
+        .through(text.utf8Decode)
+        .through(text.lines)
+        .drop(1) // remove header
+        .dropLastIf(_.isEmpty)
+        .map(mapLineToRow)
+        .map(_.toString)
+        .intersperse("\n")
+        .through(text.utf8Encode)
+        .through(io.file.writeAll(Paths.get("celsius.txt"), blocker))
+  }
+
+  def run(args: List[String]): IO[ExitCode] =
+    converter.compile.drain.map(_ => ExitCode.Success)
+
+  def mapLineToRow(line: String): Option[DataSetRow] = {
+    for {
+      fields <- mapLineToFields(line)
+      id <- fields(0).toIntOption
+    } yield
+      DataSetRow(
+        id,
+        LocalDateTime.MIN,
+        None,
+        None,
+        None,
+        0,
+        0,
+        0,
+        "",
+        None,
+        None,
+        None,
+        "",
+        0,
+        0,
+        0
+      )
+  }
+
+  def mapLineToFields(line: String): Option[List[String]] = {
+    val split = line.split(',')
+    split.length match {
+      case DataSetRow.NUMBER_OF_FIELDS => Some(split.toList)
+      case _                           => None
     }
-
-    def run(args: List[String]): IO[ExitCode] =
-        converter.compile.drain.map(_ => ExitCode.Success)
-
-    def mapLineToRow(line: String): Option[DataSetRow] = mapLineToFields(line).map(_ =>
-        DataSetRow(1, LocalDateTime.MIN, None, None, None, 0, 0, 0, "", None, None, None, "", 0, 0, 0))
-
-    def mapLineToFields(line: String): Option[List[String]] = {
-        val split = line.split(',')
-        split.length match {
-            case DataSetRow.NUMBER_OF_FIELDS => Some(split.toList)
-            case _ => None
-        }
-    }
+  }
 }
