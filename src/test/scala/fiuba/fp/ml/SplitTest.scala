2659ba68f2869d425d5dfd1022de1c445998e94f
Cecilia Hortas <chortas@fi.uba.ar>
2020-12-03 20:24:58 -0300

    Add split tests

--- src/test/scala/fiuba/fp/ml/SplitTest.scala
+++ src/test/scala/fiuba/fp/ml/SplitTest.scala
@@ -13,6 +13,30 @@ class SplitTest extends FpTpSpec {
     }
   )
 
+  "A list with ten ints" should "be splitted in two lists with 7 and 3 elements" in assertSplit(
+    List(1, 2, 3, 4, 5, 6, 7, 8, 9, 10),
+    ts => {
+      ts.train.length shouldBe 7
+      ts.test.length shouldBe 3
+    }
+  )
+
+  "A list with two ints" should "be splitted in two lists with 1 element each one" in assertSplit(
+    List(1, 2),
+    ts => {
+      ts.train.length shouldBe 1
+      ts.test.length shouldBe 1
+    }
+  )
+
+  "A list with ten strings" should "be splitted in two lists with 7 and 3 elements" in assertSplit(
+    List("", "", "", "", "", "", "", "", "", ""),
+    ts => {
+      ts.train.length shouldBe 7
+      ts.test.length shouldBe 3
+    }
+  )
+
   def assertSplit(list: List[_],
                   assertions: DataSet[_] => Assertion): Assertion =
     Split
