e99092411d629459513c8f9edbaf0355259aaf9e
Cecilia Hortas <chortas@fi.uba.ar>
2021-02-11 22:13:31 -0300

    Test score returns 200OK

--- Parte 3/build.sbt
+++ Parte 3/build.sbt
@@ -16,16 +16,20 @@ version := "0.0.1-SNAPSHOT"
 scalaVersion := "2.13.3"
 
 libraryDependencies ++= Seq(
-  "org.jpmml"          %  "pmml-evaluator"       % pmmlVersion,
-  "org.postgresql"     %  "postgresql"           % postgresVersion,
-  "ch.qos.logback"     %  "logback-classic"      % LogbackVersion,
-  "jakarta.xml.bind"   %  "jakarta.xml.bind-api" % xmlVersion,
-  "org.glassfish.jaxb" %  "jaxb-runtime"         % xmlVersion,
-  "org.http4s"         %% "http4s-blaze-server"  % Http4sVersion,
-  "org.http4s"         %% "http4s-blaze-client"  % Http4sVersion,
-  "org.http4s"         %% "http4s-circe"         % Http4sVersion,
-  "org.http4s"         %% "http4s-dsl"           % Http4sVersion,
-  "org.tpolecat"       %% "doobie-hikari"        % doobieVersion,
-  "io.circe"           %% "circe-generic"        % CirceVersion,
-  "org.specs2"         %% "specs2-core"          % Specs2Version % Test
+  "org.jpmml" % "pmml-evaluator" % pmmlVersion,
+  "org.postgresql" % "postgresql" % postgresVersion,
+  "ch.qos.logback" % "logback-classic" % LogbackVersion,
+  "jakarta.xml.bind" % "jakarta.xml.bind-api" % xmlVersion,
+  "org.glassfish.jaxb" % "jaxb-runtime" % xmlVersion,
+  "org.http4s" %% "http4s-blaze-server" % Http4sVersion,
+  "org.http4s" %% "http4s-blaze-client" % Http4sVersion,
+  "org.http4s" %% "http4s-circe" % Http4sVersion,
+  "org.http4s" %% "http4s-dsl" % Http4sVersion,
+  "org.tpolecat" %% "doobie-hikari" % doobieVersion,
+  "io.circe" %% "circe-generic" % CirceVersion,
+  "org.specs2" %% "specs2-core" % Specs2Version % Test,
+  "io.circe" %% "circe-literal" % CirceVersion % Test
 )
+
+libraryDependencies += "org.http4s" %% "http4s-json4s-native" % Http4sVersion
+libraryDependencies += "org.http4s" %% "http4s-json4s-jackson" % Http4sVersion
---
+++ Parte 3/src/test/scala/edu/fiuba/fpfiuba43/ScoresSpec.scala
@@ -0,0 +1,82 @@
+package edu.fiuba.fpfiuba43
+
+import cats.effect.{ContextShift, IO}
+import cats.implicits._
+import edu.fiuba.fpfiuba43.http.Fpfiuba43Routes
+import edu.fiuba.fpfiuba43.models.InputRow
+import edu.fiuba.fpfiuba43.services.{Pmml, ScoresImpl}
+import io.circe._
+import io.circe.literal._
+import org.http4s._
+import org.http4s.circe._
+import org.http4s.implicits._
+import org.specs2.matcher.MatchResult
+
+import scala.concurrent.ExecutionContext
+
+class ScoresSpec extends org.specs2.mutable.Specification {
+
+  implicit val cs: ContextShift[IO] = IO.contextShift(ExecutionContext.global)
+
+  "Scores" >> {
+    "return right score" >> {
+      uriReturnsRightScore()
+    }
+  }
+
+  private[this] def createRetScores(body: Json, scoreIO: IO[Double]) = {
+    val getScore = Request[IO](Method.POST, uri"/scores").withEntity(body)
+    val pmml = new Pmml[IO] {
+      override def score(inputRow: InputRow): IO[Double] = scoreIO
+    }
+    val scores = new ScoresImpl[IO](pmml)
+    Fpfiuba43Routes
+      .scoresRoutes(scores)
+      .orNotFound(getScore)
+      .unsafeRunSync()
+  }
+
+  private[this] def uriReturnsRightScore(): MatchResult[String] = {
+    val inputRow =
+      json"""{
+  "id": 158,
+  "date": "2020-12-02T14:49:15.841609",
+  "last": 0.0,
+  "close": 148.0,
+  "diff": 0.0,
+  "curr": "D",
+  "unit": "TONS",
+  "dollarBN": 2.919,
+  "dollarItau": 2.91,
+  "wDiff": -148.0
+}"""
+    val expectedScore = 2.3
+    val retScores = createRetScores(inputRow, expectedScore.pure[IO])
+    retScores.status must beEqualTo(Status.Ok)
+    retScores.as[String].unsafeRunSync() must beEqualTo(
+      s"""{"score":$expectedScore}"""
+    )
+  }
+
+  private[this] def knownHashCodeReturnsCachedValue(): MatchResult[String] = {
+    val inputRow =
+      json"""{
+  "id": 158,
+  "date": "2020-12-02T14:49:15.841609",
+  "last": 0.0,
+  "close": 148.0,
+  "diff": 0.0,
+  "curr": "D",
+  "unit": "TONS",
+  "dollarBN": 2.919,
+  "dollarItau": 2.91,
+  "wDiff": -148.0
+}"""
+    val expectedScore = 2.3
+    val retScores = createRetScores(inputRow, expectedScore.pure[IO])
+    retScores.status must beEqualTo(Status.Ok)
+    retScores.as[String].unsafeRunSync() must beEqualTo(
+      s"""{"score":$expectedScore}"""
+    )
+  }
+}
